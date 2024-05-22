92ba79c453e40c19832ae86a4abb98d52b4f8872
Andrew Gunn <andrewgunn31@gmail.com>
2024-05-22 16:57:31 -0500

    Refactor user authentication and session handling - Update Home and TopNav components for async session handling - Add UserMenu component for user actions in navigation

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,20 @@ import authConfig from './auth.config'
 import { prisma } from '@/lib/prisma'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({token, session}) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+      console.log(token)
+      console.log(session)
+      return session
+    },
+    // async jwt ({ token }) {
+    //   console.log(token)
+    //   return token
+    // },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,
---
+++ src/components/nav/UserMenu.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Session } from 'next-auth'
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+} from '@nextui-org/react'
+import Link from 'next/link'
+import { signOut } from '@/auth'
+
+type Props = {
+  user: Session['user']
+}
+export default function UserMenu ({ user }: Props) {
+  return (
+    <Dropdown placement="bottom-end">
+      <DropdownTrigger>
+        <Avatar
+          isBordered
+          as="button"
+          className="transition-transform"
+          color="secondary"
+          name={user?.name || 'user'}
+          size="sm"
+          alt={user?.name || 'user'}
+          src={user?.image || '/images/user.png'}/>
+      </DropdownTrigger>
+      <DropdownMenu variant="flat" aria-label="User actions menu">
+        <DropdownSection showDivider>
+          <DropdownItem isReadOnly as="span" className="h-14 flex flex-row"
+                        aria-label="username">
+            Signed in as <strong>{user?.name || 'user'}</strong>
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownItem as={Link} href="/members/edit">
+          Edit Profile
+        </DropdownItem>
+        <DropdownItem color="danger" onClick={async () => signOut()}
+                      href="/members/edit">
+          Log out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
