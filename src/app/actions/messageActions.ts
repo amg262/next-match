4a53c94f33fbd92f4fb062b6a738c9e401fb422a
Andrew Gunn <andrewgunn31@gmail.com>
2024-05-28 17:38:49 -0500

    Refactor message retrieval conditions, add delete functionality
    - Refactored message retrieval conditions based on container type
    - Added functionality to delete messages and update UI accordingly

--- src/app/actions/messageActions.ts
+++ src/app/actions/messageActions.ts
@@ -102,10 +102,15 @@ export async function getMessagesByContainer (container: string) {
 
     const selector = container === 'outbox' ? 'senderId' : 'recipientId'
 
+    const conditions = {
+      [container === 'outbox' ? 'senderId' : 'recipientId']: userId,
+      ...(container === 'outbox'
+        ? { senderDeleted: false }
+        : { recipientDeleted: false }),
+    }
+
     const messages = await prisma.message.findMany({
-      where: {
-        [selector]: userId,
-      },
+      where: conditions,
       orderBy: {
         createdAt: 'desc',
       },
@@ -135,6 +140,51 @@ export async function getMessagesByContainer (container: string) {
 
   } catch
     (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function deleteMessage (messageId: string, isOutbox: boolean) {
+  const selector = isOutbox ? 'senderDeleted' : 'recipientDeleted'
+
+  try {
+    const userId = await getAuthUserId()
+    await prisma.message.update({
+      where: {
+        id: messageId,
+      },
+      data: {
+        [selector]: true,
+      },
+    })
+
+    const messagesToDelete = await prisma.message.findMany({
+      where: {
+        OR: [
+          {
+            senderId: userId,
+            senderDeleted: true,
+            recipientDeleted: true,
+          },
+          {
+            recipientId: userId,
+            senderDeleted: true,
+            recipientDeleted: true,
+          },
+        ],
+      },
+    })
+
+    if (messagesToDelete.length > 0) {
+      await prisma.message.deleteMany({
+        where: {
+          OR: messagesToDelete.map(m => ({ id: m.id })),
+        },
+      })
+    }
+
+  } catch (error) {
     console.log(error)
     throw error
   }
--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter, useSearchParams } from 'next/navigation'
 import {
-  getKeyValue,
+  Avatar,
   Table,
   TableBody,
   TableCell,
@@ -11,8 +11,11 @@ import {
   TableRow,
 } from '@nextui-org/react'
 import { MessageDto } from '@/types'
-import { Key } from 'react'
+import { Key, useCallback, useState } from 'react'
 import { Card } from '@nextui-org/card'
+import { Button } from '@nextui-org/button'
+import { AiFillDelete } from 'react-icons/ai'
+import { deleteMessage } from '@/app/actions/messageActions'
 
 type Props = {
   messages: MessageDto[]
@@ -22,6 +25,7 @@ export default function MessageTable ({ messages }: Props) {
   const searchParams = useSearchParams()
   const router = useRouter()
   const isOutbox = searchParams.get('container') === 'outbox'
+  const [isDeleting, setDeleting] = useState({ id: '', loading: false })
 
   const columns = [
     {
@@ -31,7 +35,15 @@ export default function MessageTable ({ messages }: Props) {
     },
     { key: 'text', label: 'Message' },
     { key: 'createdAt', label: isOutbox ? 'Date Sent' : 'Date Received' },
+    { key: 'actions', label: 'Actions' },
   ]
+
+  const handleDeleteMessage = useCallback(async (message: MessageDto) => {
+    setDeleting({ id: message.id, loading: true })
+    await deleteMessage(message.id, isOutbox)
+    router.refresh()
+    setDeleting({ id: '', loading: false })
+  }, [isOutbox, router])
 
   const handleRowSelect = (key: Key) => {
     const message = messages.find((m) => m.id === key)
@@ -40,6 +52,40 @@ export default function MessageTable ({ messages }: Props) {
       : `/members/${message?.senderId}`
     router.push(url + '/chat')
   }
+
+  const renderCell = useCallback(
+    (item: MessageDto, columnKey: keyof MessageDto) => {
+      const cellValue = item[columnKey]
+
+      switch (columnKey) {
+        case 'recipientName':
+        case 'senderName':
+          return (
+            <div
+              className="flex items-center gap-2 cursor-pointer">
+              <Avatar alt="image of memeber" src={(isOutbox
+                ? item.recipientImage
+                : item.senderImage) || '/images/user.png'}/>
+            </div>
+          )
+        case 'text':
+          return (
+            <div className={`truncate`}>
+              {cellValue}
+            </div>
+          )
+        case 'createdAt':
+          return cellValue
+        default:
+          return (
+            <Button isIconOnly variant="light"
+                    onClick={() => handleDeleteMessage(item)}
+                    isLoading={isDeleting.id === item.id && isDeleting.loading}>
+              <AiFillDelete size={24} className="text-danger"/>
+            </Button>
+          )
+      }
+    }, [handleDeleteMessage, isDeleting.id, isDeleting.loading, isOutbox])
 
   return (
     <Card className="flex flex-col gap-3 h-[80vh] overflow-auto">
@@ -56,12 +102,9 @@ export default function MessageTable ({ messages }: Props) {
           {(item) => (
             <TableRow key={item.id} className="cursor-pointer">
               {(columnKey) => (
-                <TableCell>
-                  <div className={`${!item.dateRead && !isOutbox
-                    ? 'font-semibold'
-                    : ''}`}>
-                    {getKeyValue(item, columnKey)}
-                  </div>
+                <TableCell className={`${!item.dateRead &&
+                !isOutbox ? 'font-semibold' : ''}`}>
+                  {renderCell(item, columnKey as keyof MessageDto)}
                 </TableCell>
               )}
             </TableRow>
